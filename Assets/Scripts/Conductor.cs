04a19e78b4e361fbbeef14a38e5fb76b5dfc16ee
Sam O'Connor <samoc9549@gmail.com>
2020-04-14 15:18:22 -0400

    Added basic BeatSystem and one beat track

---
+++ Assets/Scripts/Conductor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+
+    public float beatsPerMinute;
+    public float secondsPerBeat;
+    public float songPosition;
+    public float songPositionInBeats;
+    public float dspSongTime;
+    public AudioSource source;
+
+    public float firstBeatOffset;
+
+    public float beatsPerLoop;
+    public int completedLoops = 0;
+    public float loopPositionInBeats;
+
+    public float loopPositionInAnalog;
+    public static Conductor instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        secondsPerBeat = 60f / beatsPerMinute;
+        dspSongTime = (float)AudioSettings.dspTime;
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
+        songPositionInBeats = songPosition / secondsPerBeat;
+        if (songPositionInBeats >= (completedLoops + 1) * beatsPerLoop)
+        {
+            completedLoops++;
+        }
+        loopPositionInBeats = songPositionInBeats - completedLoops * beatsPerLoop;
+        loopPositionInAnalog = loopPositionInBeats / beatsPerLoop;
+    }
+}
