2b7214374ed4ba00c352d9fcce30e05b2bea8974
Goldenthorn <alexloon123@gmail.com>
2020-04-14 20:05:38 -0400

    Text Printer and Multiplayer Setup

---
+++ Assets/NetworkConnectionManager.cs
@@ -0,0 +1,59 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkConnectionManager : MonoBehaviourPunCallbacks
+{
+    public GameObject button_master;
+    public GameObject button_room;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ConnectToMaster() {
+        PhotonNetwork.OfflineMode = false;
+        PhotonNetwork.NickName = "guest";
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "v0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void ConnectToRoom() {
+        if (!PhotonNetwork.IsConnected) {
+            return;
+        }
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause) {
+        base.OnDisconnected(cause);
+    }
+
+    public override void OnConnectedToMaster() {
+        base.OnConnectedToMaster();
+        button_master.SetActive(false);
+        button_room.SetActive(true);
+    }
+
+    public override void OnJoinedRoom() {
+        base.OnJoinedRoom();
+        SceneManager.LoadScene("MultiplayerRoom");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        base.OnJoinRandomFailed(returnCode, message);
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+    }
+}
---
+++ Assets/TextAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextAnimator : MonoBehaviour
+{
+
+    public bool animating = false;
+
+
+    public IEnumerator AnimPrint(string msg, float speed, float hang) {
+
+        //Wait for animator to finish
+        while (animating) {
+            yield return new WaitForSeconds(speed);
+        }
+
+
+        animating = true;
+        string temp = "";
+
+        for(int i = 0; i < msg.Length; i++) {
+            temp += msg[i];
+            this.GetComponent<UnityEngine.UI.Text>().text = temp;
+            yield return new WaitForSeconds(speed);
+        }
+        yield return new WaitForSeconds(hang);
+        this.GetComponent<UnityEngine.UI.Text>().text = "";
+        animating = false;
+
+    }
+
+}
